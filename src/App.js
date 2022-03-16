e5dfe7ea1e6b6de04c5b0168494eb83022ded4b1
ignamacedo <85701071+ignamacedo@users.noreply.github.com>
2022-03-16 18:40:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar.js';// importo componente NavBar.ja
+import NavBar from './components/NavBar.js';// importo componente NavBar.js
 
 
 //para llamar al componente uso etiqueta dentro de return
 function App() {
-  return ( //lo que vemos en el navegador es lo que esta adentor del return
+  return (
     <div className="conteiner">
       <header>
         <NavBar/>
